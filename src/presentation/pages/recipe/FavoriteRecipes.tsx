1ff85bb38e941a8aa95a123b8bc10f6f2a9f025f
Fr4nkD3v <frank.code31@gmail.com>
2024-07-22 10:55:35 -0500

    add font & base recipe card

--- src/presentation/pages/recipe/FavoriteRecipes.tsx
+++ src/presentation/pages/recipe/FavoriteRecipes.tsx
@@ -1,6 +1,31 @@
+import { TypographyP } from '@/presentation/components/shared/TypographyP';
+import { TypographyH1 } from '@/presentation/components/shared/TypographyH1';
+import { RecipeCard } from '@/presentation/components/recipe/RecipeCard';
+import dishImage from '/assets/dish1.png';
 
 export const FavoriteRecipes = () => {
   return (
-    <div>FavoriteRecipes</div>
+    <div>
+      <TypographyH1>Mis Favoritos</TypographyH1>
+      <TypographyP>En esta sección verás todas tus recetas favoritas</TypographyP>
+      <RecipeCard
+        imageUrl={dishImage}
+        name='Tallarines Rojos'
+        ingredientQuantity={7}
+        cookTimeInMins={30}
+        calories={230}
+        servings={2}
+        matchRate={100}
+      />
+      <RecipeCard
+        imageUrl={dishImage}
+        name='Tallarines Rojos'
+        ingredientQuantity={7}
+        cookTimeInMins={30}
+        calories={230}
+        servings={2}
+        matchRate={100}
+      />
+    </div>
   )
 }
